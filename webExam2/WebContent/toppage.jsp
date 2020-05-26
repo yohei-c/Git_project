b836cd3374f74dd02481ac74443d36bd3fad6f48
ゆかはら <y_hara@reglecasse.co.jp>
2020-05-26 11:26:02 +0900

    add toppage

---
+++ webExam2/WebContent/toppage.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>toppage</title>
+</head>
+<body>
+
+	<form action="ToppageServlet" method="get">
+		<button name="btn" value="insert">ログイン</button>
+	</form>
+	
+
+</body>
+</html>
---
+++ webExam2/src/toppageServlet/ToppageServlet.java
@@ -0,0 +1,32 @@
+package toppageServlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ToppageServlet
+ */
+@WebServlet("/ToppageServlet")
+public class ToppageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ServletContext context = getServletContext();
+		RequestDispatcher dis = context.getRequestDispatcher("/toppage.html");
+		dis.forward(request, response);
+	}
+
+
+
+}
