9bd9e94236ebe67260996dfb2ce084d247390c37
yohei-c <y-hisatomi@buddit.co.jp>
2020-05-26 15:15:00 +0900

    新規会員登録ページ作成

---
+++ webExam2/WebContent/Regist.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Regist</title>
+</head>
+<body>
+	<h2>新規会員登録</h2>
+	<form action="Regist" method="post">
+		<p>USER NAME</p>
+		<input type="text" name="name">
+		<p>PASSWORD</p>
+		<input type="password" name="pass">
+		<button>登録</button>
+	
+	</form>
+</body>
+</html>
---
+++ webExam2/src/controller/Regist.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ItemDao;
+import dto.ProfileDto;
+
+/**
+ * Servlet implementation class Regist
+ */
+@WebServlet("/Regist")
+public class Regist extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");	
+		
+		ServletContext context = getServletContext();
+		RequestDispatcher dis = context.getRequestDispatcher("/Regist.jsp");
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");	
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		
+		 ProfileDto dto = new ProfileDto();
+		 
+		 dto.setName(name);
+		 dto.setPass(pass);
+		 
+		 
+		 try {
+			ItemDao dao = new ItemDao();
+			
+			
+			if(dao.insertAccount(dto)> 0) {
+				ServletContext context = getServletContext();
+				RequestDispatcher dis = context.getRequestDispatcher("/manageTop.html");
+				dis.forward(request, response);
+				
+			}else {
+				doGet(request, response);
+			}
+		} catch (SQLException e) {
+	
+			e.printStackTrace();
+			
+		}
+		
+		
+		
+		
+	}
+
+}
